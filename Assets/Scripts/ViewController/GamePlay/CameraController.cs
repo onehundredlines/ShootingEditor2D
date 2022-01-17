d5197d231da6a2127f0dea8afe1bbeec34aa4324
Haogehaojiu <graykaras@163.com>
2022-01-18 00:24:52 +0800

    45 部分细节的完善二

--- Assets/Scripts/ViewController/GamePlay/CameraController.cs
+++ Assets/Scripts/ViewController/GamePlay/CameraController.cs
@@ -1,27 +1,36 @@
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 namespace ShootingEditor2D
 {
     public class CameraController : MonoBehaviour
     {
         private Transform mPlayerTrans;
-        private void Update()
+
+        private float xMin = -5;
+        private float xMax = 5;
+        private float yMin = -5;
+        private float yMax = 5;
+        private Vector3 mTargetPos;
+        private void LateUpdate()
         {
             if (!mPlayerTrans)
             {
                 var playerGameObj = GameObject.FindWithTag("Player").transform;
-                if (playerGameObj)
-                {
-                    mPlayerTrans = playerGameObj.transform;
-                } else
-                {
-                    return;
-                }
+                if (playerGameObj) mPlayerTrans = playerGameObj.transform;
+                else return;
             }
             var playerPos = mPlayerTrans.position;
+            // var isRight = Mathf.Sign(mPlayerTrans.transform.localScale.x);
+            // mTargetPos.x = playerPos.x + 3 * isRight;
+            mTargetPos.x = playerPos.x;
+            mTargetPos.y = playerPos.y + 1;
+            mTargetPos.z = -10;
+            var smoothSpeed = 5;
+            //增加平滑处理
             var cameraPos = transform.position;
-            cameraPos.x = playerPos.x + 2;
-            // cameraPos.y = playerPos.y + 2;
-            transform.position = cameraPos;
+            cameraPos = Vector3.Lerp(cameraPos, new Vector3(mTargetPos.x, mTargetPos.y, cameraPos.z), smoothSpeed * Time.deltaTime);
+            //锁定在一个固定区域
+            transform.position = new Vector3(Mathf.Clamp(cameraPos.x, xMin, xMax), Mathf.Clamp(cameraPos.y, yMin, yMax), cameraPos.z);
         }
     }
 }
---
+++ Assets/Scripts/ViewController/GamePlay/PlayerController.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+namespace ShootingEditor2D
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float maxAngleSpeed = 400;
+        public float minAngleSpeed = 100;
+        public float acceleration = 40;
+        public float maxMoveSpeed = 5f;
+        public float jumpSpeed = 10f;
+        public float gravity = 10f;
+        public Transform cameraTransform;
+
+        bool isOnGround;
+        bool isSecondJump;
+        float ySpeed;
+        float moveSpeed;
+        Vector3 input;
+        Vector3 move;
+        CharacterController cont;
+
+        void Start() { cont = GetComponent<CharacterController>(); }
+
+        void Update()
+        {
+            isOnGround = cont.isGrounded;
+            Drop();
+            Jump();
+            Turn();
+            Move();
+        }
+
+        void Move()
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            input.x = h;
+            input.z = v;
+
+            moveSpeed = Mathf.MoveTowards(moveSpeed, input.normalized.magnitude * maxMoveSpeed, acceleration * Time.deltaTime);
+            move = input * Time.deltaTime * moveSpeed;
+            move = this.cameraTransform.TransformDirection(move);
+            move += Vector3.up * ySpeed * Time.deltaTime;
+            
+            cont.Move(move);
+        }
+
+        void Drop()
+        {
+            if (!isOnGround)
+            {
+                ySpeed -= gravity * Time.deltaTime;
+            } else
+            {
+                if (ySpeed < -1)
+                {
+                    ySpeed += gravity * Time.deltaTime;
+                }
+                if (isSecondJump)
+                {
+                    isSecondJump = false;
+                }
+            }
+        }
+        void Jump()
+        {
+            if (isOnGround)
+            {
+                if (Input.GetButtonDown("Jump"))
+                {
+                    ySpeed = jumpSpeed;
+                }
+            } else
+            {
+                if (!isSecondJump)
+                {
+                    if (Input.GetButtonDown("Jump"))
+                    {
+                        ySpeed += jumpSpeed;
+                        isSecondJump = true;
+                    }
+                }
+            }
+        }
+
+        void Turn()
+        {
+            if (input.x != 0 || input.z != 0)
+            {
+                Vector3 targetDirection = cameraTransform.TransformDirection(input);
+                targetDirection.y = 0;
+                Quaternion lookQuaternion = Quaternion.LookRotation(targetDirection);
+                
+                float turnSpeed = Mathf.Lerp(minAngleSpeed, maxAngleSpeed, moveSpeed / maxMoveSpeed);
+
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, lookQuaternion, turnSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
