cb7ea4ad456d92c5080868da584ae2ca009883eb
Haogehaojiu <graykaras@163.com>
2022-01-17 11:12:21 +0800

    40 摄像机跟随实现 与 v0.0.3版本规划 v0.0.1 核心玩法：3C实现 主角： 左右移动（完成） 跳跃（完成） 射击（完成） 敌人： 被子弹击中消失（完成） 按键输入： 左右移动（完成） 跳跃（完成） 射击（完成） 摄像机： 固定镜头（完成）
    v0.0.2 增加目标和障碍
    统计系统：
    统计击杀数（完成）
    主角的Mode
    增加主角属性血量（完成）
    主角和敌人碰到，主角受到1点伤害（完成）
    关卡的通关功能（完成）
    摄像机跟随主角功能（完成）
    
    v0.0.3 计划
    关卡编辑器的初步实现：1、手写存储文件格式制定。2、解析并运行手写的文件
    游戏：1、增加开始界面和失败界面形成闭环。2、设置弹药数量限制，初始为3发。3、怪物增加移动。4、击败一个敌人百分之八十随机获得1～3发子弹，百分之二十不给弹药奖励。5、改善细节。
    游戏的闭环，有利于不断的去测试游戏。

---
+++ Assets/Scripts/ViewController/GamePlay/CameraController.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+namespace ShootingEditor2D
+{
+    public class CameraController : MonoBehaviour
+    {
+        private Transform mPlayerTrans;
+        private void Update()
+        {
+            if (!mPlayerTrans)
+            {
+                var playerGameObj = GameObject.FindWithTag("Player").transform;
+                if (playerGameObj)
+                {
+                    mPlayerTrans = playerGameObj.transform;
+                } else
+                {
+                    return;
+                }
+            }
+            var playerPos = mPlayerTrans.position;
+            var cameraPos = transform.position;
+            cameraPos.x = playerPos.x + 2;
+            // cameraPos.y = playerPos.y + 2;
+            transform.position = cameraPos;
+        }
+    }
+}
