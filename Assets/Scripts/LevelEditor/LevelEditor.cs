623e91cb97d595ebe415f674bb6d5033aa07300c
Haogehaojiu <graykaras@163.com>
2022-01-21 12:20:27 +0800

    73 单击绘制地块 Mathf.Floor()取整，加上0.5f，意思是四舍五入

--- Assets/Scripts/LevelEditor/LevelEditor.cs
+++ Assets/Scripts/LevelEditor/LevelEditor.cs
@@ -10,8 +10,17 @@ namespace ShootingEditor2D
             var mousePos = Input.mousePosition;
             if (!Camera.main) return;
             var mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            //Mathf.Floor()取整，加上0.5f，意思是四舍五入
+            mouseWorldPos.x = Mathf.Floor(mouseWorldPos.x + 0.5f);
+            mouseWorldPos.y = Mathf.Floor(mouseWorldPos.y + 0.5f);
             mouseWorldPos.z = 0;
             EmptyHighlight.transform.position = mouseWorldPos;
+            if (Input.GetMouseButtonDown(0))
+            {
+                var groundPrefab = Resources.Load<GameObject>("Ground");
+                var groundGameObject = Instantiate(groundPrefab, mouseWorldPos, Quaternion.identity, transform);
+                groundGameObject.name = "Ground";
+            }
         }
     }
 }
