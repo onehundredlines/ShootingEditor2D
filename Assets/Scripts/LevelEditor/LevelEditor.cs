e47d999ac247a04315c34e12029a7711f9b62f52
Haogehaojiu <graykaras@163.com>
2022-01-21 13:49:37 +0800

    74 是否重复绘制检测

--- Assets/Scripts/LevelEditor/LevelEditor.cs
+++ Assets/Scripts/LevelEditor/LevelEditor.cs
@@ -1,10 +1,16 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace ShootingEditor2D
 {
     public class LevelEditor : MonoBehaviour
     {
         public SpriteRenderer EmptyHighlight;
+        //当前高亮块坐标
+        private float mCurrentHighlightPosX;
+        private float mCurrentHighlightPosY;
+        //是否可绘制
+        private bool mCanDraw;
         private void Update()
         {
             var mousePos = Input.mousePosition;
@@ -14,8 +20,32 @@ namespace ShootingEditor2D
             mouseWorldPos.x = Mathf.Floor(mouseWorldPos.x + 0.5f);
             mouseWorldPos.y = Mathf.Floor(mouseWorldPos.y + 0.5f);
             mouseWorldPos.z = 0;
-            EmptyHighlight.transform.position = mouseWorldPos;
-            if (Input.GetMouseButtonDown(0))
+            //todo 这点没想明白，先接着学看看，回来再解决。
+            //与当前高亮块的x、y值
+            if (Math.Abs(mCurrentHighlightPosX - mouseWorldPos.x) < 0.4f && Math.Abs(mCurrentHighlightPosY - mouseWorldPos.y) < 0.4f)
+            {
+                //不做任何事
+            } else
+            {
+                //调整高亮块儿位置
+                var highlightPos = mouseWorldPos;
+                highlightPos.z = -6;
+                EmptyHighlight.transform.position = highlightPos;
+                //发出射线
+                Ray ray = Camera.main.ScreenPointToRay(mousePos);
+                var hit =  Physics2D.Raycast(ray.origin, Vector2.zero, 20);
+                //有碰撞说明有地块
+                if (hit.collider)
+                {
+                    EmptyHighlight.color=Color.red;
+                    mCanDraw = false;
+                } else
+                {
+                    EmptyHighlight.color=Color.white;
+                    mCanDraw = true; 
+                }
+            }
+            if (Input.GetMouseButtonDown(0) && mCanDraw)
             {
                 var groundPrefab = Resources.Load<GameObject>("Ground");
                 var groundGameObject = Instantiate(groundPrefab, mouseWorldPos, Quaternion.identity, transform);
